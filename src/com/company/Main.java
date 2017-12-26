75db3ff7f45ea06f1c131574130cea71981bf3cb
GGen <ggen86@gmail.com>
2017-12-26 11:37:50 +0300

    готово

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,31 +7,31 @@ public class Main {
         int i, j, l, Max = 0, Min = 0, MaxNum = -11, MinNum = 11;     //обьявили переменные
 
 
-        for (l = 0; l < 20; l++) {
-            array[l] = ((int) (Math.random() * 20) - 10);      // Цикл наполнение массива от -10 до 10 числами "рандом"
-            System.out.println(array[l]);                   // вывели что для наглядности в консоль
-        }
-
-        for (i = 0; i < 20; i++)
+        for (i = 0; i < 20; i++) {
+            array[i] = ((int) (Math.random() * 20) - 10);      // Цикл наполнение массива от -10 до 10 числами "рандом"
+            System.out.println(array[i]);                   // вывели что для наглядности в консоль
+            
             if (array[i] > MaxNum) {
                 Max = i;                                //Нашли максимальное значение
                 MaxNum = array[i];
             }
 
-        for (j = 0; j < 20; ++j)
-            if (array[j] < MinNum) {
-                Min = j;                                //Нашли минимальное значение
-                MinNum = array[j];
+            if (array[i] < MinNum) {
+                Min = i;                                //Нашли минимальное значение
+                MinNum = array[i];
             }
+        }
+                System.out.println("number Max " + array[Max] + " number Min " + array[Min]);    // вывели что для наглядности в консоль
 
-        System.out.println("number Max " + array[Max] + " number Min " + array[Min]);    // вывели что для наглядности в консоль
-        
-            int temp = array[Min];
-            array[Min] = array[Max];                   //поменяли местами значение мин и макс, первые попавшиеся
-            array[Max] = temp;
 
-   for (l = 0; l < 20; l++) {
-                                                        //вывели что для наглядности в консоль
-            System.out.println(array[l]);}
+            array[Min] = MaxNum;                   //поменяли местами значение мин и макс, первые попавшиеся
+            array[Max] = MinNum;
+
+
+   for (i = 0; i < 20; i++) {
+
+            System.out.println(array[i]);           //вывели что для наглядности в консоль
+   }
+
             }
 }
