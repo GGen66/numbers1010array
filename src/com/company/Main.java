bf5557f3aa38f640d3650411d8bccc6bbe54514e
GGen <ggen86@gmail.com>
2017-12-26 19:39:07 +0300

    готово

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,13 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
         int[] array = new int[20];                                  //создали  массив
-        int i, j, l, Max = 0, Min = 0, MaxNum = -11, MinNum = 11;     //обьявили переменные
+        int i, Max = 0, Min = 0, MaxNum = -11, MinNum = 11;     //обьявили переменные
 
 
         for (i = 0; i < 20; i++) {
             array[i] = ((int) (Math.random() * 20) - 10);      // Цикл наполнение массива от -10 до 10 числами "рандом"
-            System.out.println(array[i]);                   // вывели что для наглядности в консоль
-            
+            System.out.println("Mass[" + i+ "]= " + array[i]);                   // вывели что для наглядности в консоль
+
             if (array[i] > MaxNum) {
                 Max = i;                                //Нашли максимальное значение
                 MaxNum = array[i];
@@ -21,17 +21,18 @@ public class Main {
                 MinNum = array[i];
             }
         }
-                System.out.println("number Max " + array[Max] + " number Min " + array[Min]);    // вывели что для наглядности в консоль
+        System.out.println("number Max " + array[Max] + " number Min " + array[Min]);    // вывели что для наглядности в консоль
 
 
-            array[Min] = MaxNum;                   //поменяли местами значение мин и макс, первые попавшиеся
-            array[Max] = MinNum;
+        array[Min] = MaxNum;                   //поменяли местами значение мин и макс, первые попавшиеся
+        array[Max] = MinNum;
 
 
-   for (i = 0; i < 20; i++) {
+        for (i = 0; i < 20; i++) {
 
-            System.out.println(array[i]);           //вывели что для наглядности в консоль
-   }
+            System.out.println("Mass[" + i + "]= " + array[i]);           //вывели что для наглядности в консоль
+        }
 
-            }
+    }
 }
+
